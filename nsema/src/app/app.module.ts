affbd044ba4d9f0a42da25d7e31775f3aece3303
PLDiCicco <peter@nsmithfieldri.org>
2017-01-19 11:59:11 -0500

    Remove Page1

--- nsema/src/app/app.module.ts
+++ nsema/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { HomePage } from '../pages/home/home';
 
@@ -9,7 +8,6 @@ import { HomePage } from '../pages/home/home';
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
     Page2,
     HomePage
   ],
@@ -19,7 +17,6 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
     Page2,
     HomePage
   ],
